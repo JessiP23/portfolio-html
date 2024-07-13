07e444344435bc6e07a6f951cbe130843819cf10
JessiP23 <jessi316866@gmail.com>
2024-07-12 20:38:37 -0400

    portfolio sample finished

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const header = document.querySelector(".navbar");
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
 
-// fires up whtn the scroll event happen
 window.onscroll = function() {
-    // Returns the number of pixels that the document is scrolled vertically and its value is assigned to a variable named top
     var top = window.scrollY;
-    // If the value of top is greater than or equal to 100, it adds a class of navbarDark to the header.
-    if (top >= 100) {
-        header.classList.add("navbarDark");
-    } else {
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
         header.classList.remove('navbarDark');
     }
 }
